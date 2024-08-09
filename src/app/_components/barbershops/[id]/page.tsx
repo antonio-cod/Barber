0d66056c7c2de8948f995648ef6ed168f4b80dac
antonio-cod <antonio_carlostads@hotmail.com>
2024-08-09 11:10:39 -0400

    feat:add Phoneitem component and Footer to layout

--- src/app/_components/barbershops/[id]/page.tsx
+++ src/app/_components/barbershops/[id]/page.tsx
@@ -1,5 +1,3 @@
-import PhoneItem from "@/app/_components/phone-item"
-import ServiceItem from "@/app/_components/service-item"
 import SidebarSheet from "@/app/_components/sidebar-sheet"
 import { Button } from "@/app/_components/ui/button"
 import { Sheet, SheetTrigger } from "@/app/_components/ui/sheet"
@@ -8,6 +6,8 @@ import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { notFound } from "next/navigation"
+import PhoneItem from "../../phone-item"
+import ServiceItem from "../../service-item"
 
 interface BarbershopPageProps {
   params: {
